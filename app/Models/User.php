1996bd18fe577bdd5e54bf1bf8330c1986a55766
Jenary Madia <jenary.madia@rebisco.com.ph>
2024-08-29 16:46:50 +0800

    add relationship for team and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,5 +52,13 @@ class User extends Authenticatable
     {
         return $this->hasOne(Team::class, 'user_id')
                 ->where('personal_team', 1);
+    }
+
+    /**
+     * The teams that the user belongs to.
+     */
+    public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Team::class, 'team_user');
     }
 }
